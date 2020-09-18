1664c4e4d254958253a627733ad4e9926075534c
Justin D Selbo <selboj@ecegrid-thin1.ecn.purdue.edu>
2020-09-18 01:51:03 -0400

    modified:   HW05Sum/fileint.c 	new file:   HW05Sum/fileint.o 	new file:   HW05Sum/hw05 	new file:   HW05Sum/main.o 	new file:   HW05Sum/output1 	new file:   HW05Sum/output2 	new file:   HW05Sum/output3 	new file:   HW05Sum/output4 	new file:   HW05Sum/output5 	new file:   HW05Sum/output6

--- HW05Sum/fileint.c
+++ HW05Sum/fileint.c
@@ -16,6 +16,26 @@ bool addFile(char * filename, int * sum)
   // * sum stores the result of adding all numbers from the file
   // When no more numbers can be read, fclose, return true
   //
+  
+  int num;
+  * sum = 0;
+
+  FILE * readFile = fopen(filename, "r");
+  
+  //make sure file openes correctly
+  if (readFile == NULL) 
+  {
+    return false;
+  }
+  
+  //read and add the numbers
+  while(fscanf(readFile, "%d", & num) == 1)
+    {
+      * sum += num;
+    }
+  
+  fclose(readFile);
+
   return true;
 }
 #endif
@@ -29,6 +49,20 @@ bool writeSum(char * filename, int sum)
   // fprintf should use one newline '\n'
   // fclose, return true
   //
+
+  FILE * writeFile = fopen(filename, "w");
+  
+  //make sure it opened for writing
+  if (writeFile == NULL)
+    {
+      return false;
+    }
+  
+  fprintf(writeFile, "%d", sum);
+
+  //close file
+  fclose(writeFile);
+
   return true;
 }
 #endif
