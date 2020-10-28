4f6d8fada795651d91621134240e42dd674267de
Justin D Selbo <selboj@ecegrid-thin1.ecn.purdue.edu>
2020-10-27 23:05:44 -0400

    new file:   Makefile 	modified:   calculate.c 	modified:   list.c

--- HW13Arithmetic1/calculate.c
+++ HW13Arithmetic1/calculate.c
@@ -48,6 +48,14 @@ bool calculate(List * arithlist)
     {
       return true;
     }
+  
+  //Checks to see if the input list is valid
+  ListNode * iterNode = arithlist -> head;
+  int numOperands = 0;
+  if (isOperator(iterNode -> word) > 0) {
+    return false;
+  }
+
   // go through the list until there is only node in the list
   // find the next operator
   // If no operator can be found, return false
@@ -63,14 +71,79 @@ bool calculate(List * arithlist)
   //     the list should have exactly one node left. If this is not
   //     true, return false
   // If the input is valud, return true
+  
+  ListNode * curNode = arithlist -> tail;
+  ListNode * searchNode = curNode -> prev;
 
+  int numOperands = 0;
+  int i = 0;
+  char * nonNums;
+  int operand = 0;
+  ListNode * operand1 = NULL;
+  ListNode * operand2 = NULL;
+  int operandNum1 = 0;
+  int operandNum2 = 0;
+  int operator = 0;
+  int newNum = 0;
 
+  while (arithlist -> head != arithlist -> tail && curNode != NULL) {
+    if (isOperator(curNode) > -1) {
+      //Find next two operands
+      ListNode * operatorNode = curNode;
+      operator = isOperator(operatorNode);
+      i = 0;
+      numOperands = 0;
+      searchNode = curNode -> prev;
+      while(searchNode != NULL && numOperands != 2) {
+        if (isOperator(searchNode) == -1) {
+          if (numOperands == 0) {operand1 = searchNode;}
+          else {operand2 = searchNode;}
+          numOperands++;
+          i++;
+        }
+        searchNode = searchNode -> prev;
+      }
+    }
+    if (seardNode == NULL && numOperands != 2) {
+      //There are not two available operands for the operators
+      return false;
+    }
+    else {
+      //Check to make sure only integers, and not other characters
+      operandNum1 = strtol(operand1 -> word, &nonNums, 10);
+      operandNum2 = strtol(operand2 -> word, &nonNums, 10);
+
+      //Do the required maths
+      if (operator == 0) {
+        newNum = operandNum1 + operandNum2;
+        sprintf(operand2 -> word, "%d", newNum); //AFUNADIUGNAIUGDNAD LOOK RIGHT HERE NOT STORED CORRECTLY
+        deleteNode(arithlist, operand1);
+        deleteNode(arithlist, operatorNode);
+      }
+      else if (operator == 1) {
+        newNum = operandNum2 - operandNum1;
+        sprintf(operand2 -> word, "%d", newNum); //AFUNADIUGNAIUGDNAD LOOK RIGHT HERE NOT STORED CORRECTLY
+        deleteNode(arithlist, operand1);
+        deleteNode(arithlist, operatorNode);
+      }
+      else if (operator == 2) {
+        newNum = operandNum2 * operandNum1;
+        operand2 -> word = newNum //AFUNADIUGNAIUGDNAD LOOK RIGHT HERE NOT STORED CORRECTLY
+        deleteNode(arithlist, operand1);
+        deleteNode(arithlist, operatorNode);
+      }
+    }
+    curNode = curNode -> next;
+  }
   
   // if more than one node left, return false
+  if (arithlist -> head != arithlist -> tail) {return false;}
 
   // if the remaining node is an operator, return false
+  if (isOperator(arithlist -> head) != -1) {return false;}
 
   // if everything is OK, return true
+  deleteList(arithlist);
   return true;
 }
 #endif
--- HW13Arithmetic1/list.c
+++ HW13Arithmetic1/list.c
@@ -12,7 +12,7 @@
 // each line shorter than WORDLENGTH (including '\n' and '\0')
 // arithlist should have memory to store head and tail
 // If arithlist is NULL, return false
-// If fopen fails, return false
+// If fopen fails, return false 
 // If a line is too long,
 //    free memory of the list
 //    fclose
@@ -23,6 +23,29 @@
 //    return true
 bool readList(char * filename, List * arithlist)
 {
+  //File open
+  FILE * readFile = fopen(filename, "r");
+  if (readFile == NULL) {return false;}
+
+  //Make sure arithlist is useable and not NULL
+  if (arithlist == NULL) {return false;}
+
+  char tempword[WORDLENGTH];
+
+  while (!feof(readFile)) {
+    if (fgets(tempword, WORDLENGTH, readFile) != NULL) {
+      //Add line as a new node to the arithlist
+      addnode(arithlist, tempword);
+    }
+    else {
+      //Free and close
+      deletelist(arithlist);
+      fclose(readFile);
+      return false;
+    }
+  }
+
+  fclose(readFile);
   return true;
 }
 #endif
@@ -33,6 +56,15 @@ bool readList(char * filename, List * arithlist)
 // release the memory of the list 
 void deleteList(List * arithlist)
 {
+  if (arithlist == NULL) {return;}
+
+  ListNode * head = arithlits -> head;
+
+  while (head != NULL) {
+    Node * nextNode = head -> next;
+    free(head);
+    head = nextNode;
+  }
 }
 #endif
 
@@ -50,6 +82,36 @@ void deleteList(List * arithlist)
 // insert the ListNode to the list
 void addNode(List * arithlist, char * word)
 {
+  if (arithlist == NULL) {return;}
+
+  //Create new node with word
+  ListNode * newNode = malloc(WORDLENGTH);
+  newNode -> word = {0};
+  strcpy(nedNode -> word[WORDLENGTH], word);
+  //newNode -> word = word;
+
+  //Check to see if a node exists
+  if (arithlist -> head == NULL && arithlist -> tail == NULL) {
+    //no nodes exist, set prev and next to null
+     newNode -> prev == NULL;
+     newNode -> next == NULL;
+    
+    //Update arithlist
+     arithlist -> head == newNode;
+     arithlist -> tail == newNode;
+  }
+  else {
+    //get last node
+    ListNode * lastNode = arithlist -> tail;
+
+    //Set the new node as the last node
+    lastNode -> next = newNode;
+    newNode -> prev = lastNode;
+    newNode -> next = NULL;
+
+    //Set arithlist to point to new tail
+    arithlist -> tail = newNode;
+  }
 }
 #endif
 
@@ -69,7 +131,44 @@ void addNode(List * arithlist, char * word)
 // Be careful about delete the first or the last node
 bool deleteNode(List * arithlist, ListNode * ln)
 {
-  return true;
+  if (arithlist == NULL) {return false;}
+  if (artihlist -> head == NULL && arithlist -> tail == NULL) {return false;}
+
+  ListNode * head = arithlist -> head;
+  ListNode * tail = arithlist -> tail;
+
+  if (head -> word == ln -> word) {
+    //Set next node up to be new head, free original head, and update arithlist
+    ListNode * nextNode = head -> next;
+    nextNode -> prev = NULL;
+    free(head);
+    arithlist -> head = nextNode;
+    return true;
+  }
+  else if (tail -> word == ln -> word) {
+    //Set prev node to be new tail, free original tail, and udpdate arithlist
+    ListNode * prevNode = tail -> prev;
+    prevNode -> next = NULL;
+    free(tail);
+    arithlist -> tail = prevNode;
+    return true;
+  }
+  else {
+    ListNode * p = tail;
+    ListNode * q = p -> prev;
+    while ((q != NULL) && ((q -> word) != (ln -> word))) {
+      p = p -> prev;
+      q = q -> prev;
+    }
+    if (q != NULL) {
+      ListNode * nextNode = q -> prev;
+      p -> prev = nextNode;
+      nextNode -> next = p;
+      free (q);
+      return true;
+    }
+    return false;
+  }
+  //return true;
 }
 #endif
-
